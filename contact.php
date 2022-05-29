1ae5f43de9db837eafd377fe51cdf32f255be608
Kera Cudmore <kera.cudmore@hotmail.com>
2022-05-29 23:52:32 +0100

    initial set up of file with form inputs

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+</head>
+
+<body>
+<h1>Contact Us</h1>
+
+<p>Please use the form below to contact us.</p>
+
+<form class="form-container" action="" method="POST">
+
+<label for="name">Name</label>
+<input type="text" id="name" name="name">
+
+<label for="email">Email</label>
+<input type="email" id="email" name="email">
+
+<label for="enquiry">Reason for enquiry</label>
+<input type="text" id="enquiry" name="enquiry">
+
+<label for="message">Message</label>
+<textarea id="message" name="message"></textarea>
+
+<button type="submit" name="submit">Submit Enquiry</button>
+
+</form>
+
+</body>
+</html>
